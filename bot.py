b59b9b834eaf30828d3cc7e84d14c5076677c1dd
MohitJain0115 <150508372+MohitJain0115@users.noreply.github.com>
2026-02-18 21:53:59 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import feedparser
 import requests
 
-TOKEN = "8241752968:AAGISvzpSDSPMLS_FxQOHkobg9T-23SBj04"
+TOKEN = "8241752968:AAEM38E7zxFlU9bEw1i4z8B8kxDW2a9pMvc"
 CHAT_ID = "1003508880606"
 
 RSS_URL = "https://rss.app/feeds/urVv1QL6bZI0zoAF.xml"
